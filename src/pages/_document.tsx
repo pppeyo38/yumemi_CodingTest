c49761edc3d3bc2415b86fc7249280c764847880
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-10-31 23:21:17 +0900

    TypeScriptの導入

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import type { DocumentContext, DocumentInitialProps } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          <meta charSet="utf-8" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>タイトル</title>
+      </Head>
+
+      <p>トップページ</p>
+    </>
+  );
+};
+
+export default Home;
