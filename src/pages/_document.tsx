8bfdef944ac6afc40a25a7a42bcddb2cb0621a47
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-11-01 00:13:09 +0900

    add Prettier & update ESLint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,24 @@
-import type { DocumentContext, DocumentInitialProps } from "next/document";
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import type { DocumentContext, DocumentInitialProps } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   static async getInitialProps(
-    ctx: DocumentContext
+    ctx: DocumentContext,
   ): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
 
   render() {
     return (
-      <Html lang="ja">
+      <Html lang='ja'>
         <Head>
-          <meta charSet="utf-8" />
+          <meta charSet='utf-8' />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
 
       <p>トップページ</p>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
