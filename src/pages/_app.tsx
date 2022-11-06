8928b3a0f56d4d9022ca71da22a71a7a3d36cf9c
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-11-06 13:02:29 +0900

    add emotionReset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
+import { css, Global } from '@emotion/react'
+import emotionReset from 'emotion-reset'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          ${emotionReset}
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
